e59623ff805a4f39a2a8d53c6df086f1736a832b
Maximilian Ruta <mr@xtain.net>
2014-07-27 14:03:25 +0200

    created install script

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd deployment/
-tar cf - * | ssh root@$1 '(cd /; tar xf - )'
---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Example: ./install.sh 172.16.1.1 wrt-individual-mruta "role[micro-router]"
+
+if [ -f "deployment/etc/chef/config.json" ]; then
+  rm deployment/etc/chef/*
+fi
+
+server="https://chef.xtain.net"
+node="${1}"
+nodename="${2}"
+runlist="${3}"
+privatekeypath="${4}"
+
+create-config() {
+  if [ -z "${nodename}" ]; then
+    read -p "Node name: " nodename
+  fi
+  if [ "${nodename}" ]; then
+    knife node show -E --no-color "${nodename}" > /dev/null 2>&1
+    if [ "${?}" -eq "100" ]; then
+      knife node create -d "${nodename}"
+    fi;
+    knife client show --no-color "${nodename}" > /dev/null 2>&1
+    if [ "${?}" -eq "100" ]; then
+      PRIVATEKEY=$(knife client create -d "${nodename}" | tail -n +2)
+    else
+      while [ -z "${privatekeypath}" ]; do
+        read -p "Client for '${nodename}' already exists. Please give me the path to the private key: " privatekeypath
+        if [ "${privatekeypath}" ] && ! [ -f "${privatekeypath}" ]; then
+        	echo "'${privatekeypath}' is not a file."
+        	privatekeypath=""
+        fi
+      done
+      PRIVATEKEY=$(cat "${privatekeypath}")
+    fi;
+
+    if [ -z "${runlist}" ]; then
+      read -p "Run list (comma seperated): " runlist
+    fi
+    if [ "${runlist}" ]; then
+    	knife node run_list set "${nodename}" "${runlist}" > /dev/null
+    fi
+
+    cat > deployment/etc/chef/private.pem <<EOF
+${PRIVATEKEY}
+EOF
+    cat > deployment/etc/chef/config.json <<EOF
+{
+	"chef": {
+		"client": {
+			"name": "${nodename}",
+			"key": "/etc/chef/private.pem"
+		},
+		"server": "${server}",
+		"node": {
+			"name": "${nodename}"
+		}
+	},
+	"cookbooks": "/opt/chef/cookbooks"
+}
+EOF
+  fi
+}
+
+if [ -z "${nodename}" ]; then
+  while true; do
+    read -p "Do you want to create an config file? [Y/N] " yn
+    case $yn in
+      [Yy]* ) create-config; break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer with yes or no.";;
+    esac
+  done
+else
+  create-config
+fi
+
+cd deployment/
+tar cf - * | ssh root@${node} '(cd /; tar xf - ; /opt/chef/setup)'
+
+if [ -f "deployment/etc/chef/config.json" ]; then
+  rm deployment/etc/chef/*
+fi
